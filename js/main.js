27912ee244f07dfa0d3cc1918c83bd7b3165235c
SJuras <jurasstpko@gmailcom>
2021-11-09 01:49:12 +0100

    v1.1.7 added a responsive navigation menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ window.addEventListener('scroll', function(){
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 // sticky header END
+
+// toggle menu
+function toggleMenu(){
+  const sidemenu = document.querySelector(".navigation");
+  const toggle = document.querySelector(".toggle");
+  sidemenu.classList.toggle("active");
+  toggle.classList.toggle("active");
+}
+// toggle menu END
